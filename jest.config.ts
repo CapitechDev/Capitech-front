d9b6f9ef91141620a696d8af26973d418f4a4bd9
Joel Neto <joelalvesneto187@gmail.com>
2024-12-01 16:47:21 -0300

    ✨ feat(jest): update coverage configuration for improved reporting and exclusions

--- jest.config.ts
+++ jest.config.ts
@@ -12,24 +12,61 @@ const config: Config = {
   testEnvironment: "jsdom",
   // Add more setup options before each test is run
   // setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
-  
+
   // Configurações de cobertura
   collectCoverage: true,
+  // collectCoverageFrom: [
+  //   // Incluir apenas arquivos relevantes, como componentes e páginas
+  //   "app/**/*.{js,jsx,ts,tsx}",
+
+  //   // Excluir arquivos de definição de tipos e arquivos de configuração
+  //   "!app/api/**/*.{js,jsx,ts,tsx}",
+  //   "!components/**/*.{js,jsx,ts,tsx}",
+  //   "!hooks/**/*.{js,jsx,ts,tsx}",
+  //   "!lib/**/*.{js,jsx,ts,tsx}",
+  //   "!services/**/*.{js,jsx,ts,tsx}",
+  //   "!validations/**/*.{js,jsx,ts,tsx}",
+  //   "!**/*.d.ts", // Excluir arquivos de tipo TypeScript
+  //   "!**/node_modules/**", // Excluir o diretório node_modules
+  //   "!**/.next/**", // Excluir o diretório .next gerado pelo Next.js
+  //   "!**/jest.config.ts", // Excluir o arquivo de configuração do Jest
+  //   "!**/next.config.js", // Excluir o arquivo de configuração do Next.js
+  //   "!**/tailwind.config.js", // Excluir o arquivo de configuração do Tailwind
+  //   "!**/postcss.config.js", // Excluir o arquivo de configuração do PostCSS
+  //   "!**/pages/_app.tsx", // Excluir o arquivo de configuração do Next.js
+  //   "!**/pages/_document.tsx", // Excluir o arquivo de configuração do Next.js
+  // ],
   collectCoverageFrom: [
-    'src/**/*.{js,jsx,ts,tsx}',  // Ajuste o caminho conforme sua estrutura de projeto
-    '!src/**/*.d.ts',             // Exclui arquivos de definição de tipos
-    '!src/pages/_app.tsx',        // Exclui arquivos específicos se necessário
-    '!src/pages/_document.tsx'
+    "app/**/*.{js,jsx,ts,tsx}", // Ajuste para a pasta onde o código está
+    "!app/**/*.d.ts", // Exclui arquivos de definição de tipos
+    "!app/pages/_app.tsx", // Exclui arquivos específicos
+    "!app/pages/_document.tsx",
+    "!app/api/**/*.{js,jsx,ts,tsx}", // Exclui arquivos de API
   ],
-  coverageReporters: ['text', 'lcov', 'html'],
+
+  coverageReporters: ["text", "lcov", "html"],
   coverageThreshold: {
     global: {
       branches: 80,
       functions: 80,
       lines: 80,
-      statements: 80
-    }
-  }
+      statements: 80,
+    },
+  },
+  // coveragePathIgnorePatterns: [
+  //   "/node_modules/",
+  //   "/\\.next/",
+  //   "/jest.config.ts",
+  //   "/next.config.js",
+  //   "/tailwind.config.js",
+  //   "/postcss.config.js",
+  //   "/pages/_app.tsx",
+  //   "/pages/_document.tsx",
+  // ],
+  coveragePathIgnorePatterns: [
+    "/node_modules/", // Ignora arquivos dentro do diretório node_modules
+    "/\\.next/", // Ignora arquivos da pasta .next (onde o Next.js gera arquivos compilados)
+  ],
 };
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
