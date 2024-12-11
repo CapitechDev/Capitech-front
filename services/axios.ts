b284d75e5ad7ace9374e132d08fab0df14930bcd
Joel Neto <joelalvesneto187@gmail.com>
2024-12-10 21:16:57 -0300

    :bug: Fix: general feedbacks

--- services/axios.ts
+++ services/axios.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { getSession } from "next-auth/react";
 
 const api = axios.create({
-  baseURL: "http://localhost:4000",
+  baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:4000",
 });
 
 api.interceptors.request.use(async (request) => {
