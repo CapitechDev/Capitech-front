4033f43c5ecca2854690629c19dbb7250221d283
Joel Neto <joelalvesneto187@gmail.com>
2025-11-27 21:45:34 -0300

    :art: refactor: simplify image selection logic and add missing trail images for express and react

--- components/UI/atoms/homeTrailImage.tsx
+++ components/UI/atoms/homeTrailImage.tsx
@@ -5,22 +5,26 @@ interface IHomeTrailImageProps {
   className?: string;
 }
 
+const imagesRecord: Record<string, string>  = {
+  html5: "html.png",
+  html: "html.png",
+  javascript: "javascript.png", 
+  css: "css.png",
+  sql: "sql.png",
+  reactjs: "react.png",
+  react: "react.png",
+  express: "express.png"
+}
+
 export const HomeTrailImage = ({ trailName, className = "" }: IHomeTrailImageProps) => {
   const chooseImage = (name: string) => {
-    switch (name.toLowerCase()) {
-      case "html5":
-        return "/assets/trails/html.png";
-      case "html":
-        return "/assets/trails/html.png";
-      case "javascript":
-        return "/assets/trails/javascript.png";
-      case "css":
-        return "/assets/trails/css.png";
-      case "sql":
-        return "/assets/trails/sql.png";
-      default:
-        return "/assets/logo.png";
+    const nameLowerCase = name.toLowerCase();
+    if (imagesRecord[nameLowerCase]) {
+      const img = imagesRecord[nameLowerCase];
+      console.log(`/assets/trails/${img}`)
+      return `/assets/trails/${img}`;
     }
+    return "/assets/logo.png"
   };
 
   return (
